70746979906acee3d2d9571892cda1a91f11c3b7
brenthuang <guang11cheng@qq.com>
2019-04-21 20:29:15 +0800

    更新第三方库

--- base/addr2line.py
+++ base/addr2line.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-a = 0xB902505  # valgrind报的地址
-b = 0x0b8b6000  # maps文件中模块的基址，注意要在前面加0x
+a = 0xABA4554  # valgrind报的地址
+b = 0x0ab96000  # maps文件中模块(.so)的基址，注意要在前面加0x
 
 c = a - b
 print('%#x' % c)  # 0x63efd
