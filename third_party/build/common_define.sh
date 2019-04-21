6695960a16de258211aa4e6d21d75324467b439b
brenthuang <guang11cheng@qq.com>
2019-04-21 20:35:04 +0800

    更新第三方库

--- third_party/build/common_define.sh
+++ third_party/build/common_define.sh
@@ -4,12 +4,12 @@
 # author: BrentHuang (guang11cheng@qq.com)
 ###############################################################################
 
-XLRD_BASENAME=xlrd-1.1.0
-XLSXWRITER_BASENAME=XlsxWriter-1.1.2
+XLRD_BASENAME=xlrd-1.2.0
+XLSXWRITER_BASENAME=XlsxWriter-1.1.7
 NOSE_BASENAME=nose-1.3.7
 CHARDET_BASENAME=chardet-3.0.4
 CONCURRENT_LOG_HANDLER_BASENAME=ConcurrentLogHandler-0.9.1
-WEBSOCKET_CLIENT_BASENAME=websocket_client-0.54.0
+WEBSOCKET_CLIENT_BASENAME=websocket_client-0.56.0
 
 XLRD_SRC_DIR=${ARCHIVES_DIR}/${XLRD_BASENAME}
 XLSXWRITER_SRC_DIR=${ARCHIVES_DIR}/${XLSXWRITER_BASENAME}
