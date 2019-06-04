b2af101f1f4e5107adcb10f865ef8c578d3a6005
hgc <guang11cheng@qq.com>
2019-06-04 08:57:39 +0800

    修复centos上编译urllib3报错的bug

--- third_party/build/sys_prepare.sh
+++ third_party/build/sys_prepare.sh
@@ -14,6 +14,11 @@ if [ $(command -v apt-get) ]; then
     wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 elif [ $(command -v yum) ]; then
     yum install python-setuptools -y
+
+    yum -y install epel-release # 安装epel扩展源
+    yum -y install python-pip # 安装pip
+    yum clean all # 清除cache
+    pip install --upgrade setuptools # 'extras_require' must be a dictionary whose values are strings or lists of strings containing valid project/version requirement specifiers.
 else
     echo "not supported os"
     exit 1
